d7f06d20d33a94df77efa3df9601bc6c4b621b71
Lokesh Balaji <lokesh.b07@gmail.com>
2025-08-28 13:37:17 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // replace REPO_NAME with your GitHub repo name
 export default defineConfig({
   plugins: [react()],
-  base: '/REPO_NAME/',
+  base: "/portfolio/",
 })
